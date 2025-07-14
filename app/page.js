1d10f5225e55f7609d5c81b5edaa93c61f68b191
Zahidul-Turja <zahidulturja@gmail.com>
2025-07-14 16:24:33 +0600

    Update welcome message on HomePage to indicate testing environment

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <main style={{ padding: "2rem" }}>
-      <h1>Welcome to SpyTube</h1>
+      <h1>Welcome to SpyTube testing</h1>
       <a href="/login">Go to Login</a>
     </main>
   );
